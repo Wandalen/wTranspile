4d3952a03483cec7798ae77ed2f579e0d3b934b2
Wandalen <wandalen.me@gmail.com>
2019-07-12 14:56:13 +0300

    auto commit

---
+++ out/debug/dwtools/atop/transpilationStrategy/IncludeBase.s
@@ -0,0 +1,17 @@
+( function IncludeBase_s_( ) {
+
+'use strict';
+
+if( typeof module !== 'undefined' )
+{
+
+  let _ = require( '../../Tools.s' );
+
+  _.include( 'wCopyable' );
+  _.include( 'wPathFundamentals' );
+  _.include( 'wVerbal' );
+  _.include( 'wFiles' );
+
+}
+
+})();
