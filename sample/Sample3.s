1b3d1a9ac2bf73da59be6cd1c2ae692c9c51e96a
volodymyrmigdal <volodymyr.migdal@gmail.com>
2020-09-10 19:47:45 +0300

    repair Closure strategy

---
+++ sample/Sample3.s
@@ -0,0 +1,23 @@
+
+let _ = require( 'wTools' );
+require( 'wtranspile' );
+let ts = new _.trs.System().form();
+
+let outPath = _.path.join( __dirname, '../temp.tmp/Sample3.s' );
+
+_.fileProvider.filesDelete( outPath )
+
+let multiple = ts.multiple
+({
+  inPath : __filename,
+  outPath : outPath,
+  transpilingStrategy : [ 'Closure' ],
+});
+
+return multiple.form().perform()
+.finally( ( err,got ) =>
+{
+  if( err )
+  _.errLogOnce( err );
+  return got;
+});
